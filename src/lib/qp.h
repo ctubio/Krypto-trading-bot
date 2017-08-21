fa9535e048f830ba26284e23d798b3d49ba73b2b
Zak Thompson <zakthompson@ZbookPRO.local>
2017-08-21 14:06:06 -0400

    one more for good luck!

--- src/lib/qp.h
+++ src/lib/qp.h
@@ -66,7 +66,7 @@ namespace K {
     {  "aspactive",                     false                                  },
     {  "aspvalue",                      decimal_cast<1>("0").getAsDouble()     }
 
-  };
+  }; 
   static vector<string> boolQP = {
     "widthPercentage", "bestWidth", "sellSizeMax", "buySizeMax", "percentageValues",
     "quotingEwmaProtection", "quotingStdevBollingerBands", "cancelOrdersAuto", "audio"
